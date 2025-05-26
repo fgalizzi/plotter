b132544fca4b315de4af87b23b4b3c6b5e8218f9
fgalizzi <fedegal99@gmail.com>
2025-05-26 12:04:04 +0200

    Add functionalisties to handle histos and tgraphs

--- Utils.hpp
+++ Utils.hpp
@@ -1,5 +1,6 @@
 // ROOT
 #include "TGraph.h"
+#include "TMultiGraph.h"
 #include "TH1.h"
 #include "TColor.h"
 #include "TStyle.h"
@@ -8,10 +9,19 @@
 #include <TLegend.h>
 #include <TLegendEntry.h>
 #include <TH1.h>
+#include <TMath.h>
+#include <TEfficiency.h>
+#include "TGraphErrors.h"
+#include "TGraphAsymmErrors.h"
 #include "Rtypes.h"
 // GENERAL
 #include <cstddef>
+#include <vector>
 #include <iostream>
+
+
+// --- STYLE --------------------------------------------------------
+int line_width = 6;
 
 int canvas_width = 1200;
 int canvas_height = 900;
@@ -63,8 +73,59 @@ inline void CenterTitles(TH1 *histo)
     histo->GetZaxis()->CenterTitle();
   }
 
-  inline void CenterTitles(TGraph *histo)
+inline void CenterTitles(TGraph *histo)
   {
     histo->GetXaxis()->CenterTitle();
     histo->GetYaxis()->CenterTitle();
   }
+
+
+
+template<typename T>
+void vector_roll(std::vector<T>& vec, int roll){
+  if (roll > 0) std::rotate(vec.begin(), vec.begin()+vec.size()-roll, vec.end());
+  if (roll < 0) std::rotate(vec.begin(), vec.begin()-roll, vec.end());
+  return;
+}
+
+// --- HISTOS -------------------------------------------------------
+template<typename T>
+void make_histo_cute(T h, int line_width = 6,
+                        int idx_color = 1,
+                        double fill_alpha = 0.,
+                        bool show_marker = false){
+        h->SetLineWidth(line_width);
+        h->SetLineColor(color_list[idx_color % color_list.size()].GetNumber());
+        h->SetMarkerColor(color_list[idx_color % color_list.size()].GetNumber());
+        h->SetFillColorAlpha(color_list[idx_color % color_list.size()].GetNumber(), fill_alpha);
+
+        if (show_marker){
+          h->SetMarkerStyle(21);
+          h->SetLineColorAlpha(color_list[idx_color % color_list.size()].GetNumber(),  0.5);
+        }
+
+      }
+
+// --- GRAPHS -------------------------------------------------------
+TGraph* g_allign_maximum(TGraph* g, double max_position_percentage){
+  std::vector<double> y, x;
+
+  int n = g->GetN();
+
+  for (int i = 0; i < n; ++i) {
+    x.push_back(g->GetX()[i]);
+    y.push_back(g->GetY()[i]);
+  }
+
+  int max_index = std::distance(y.begin(), std::max_element(y.begin(), y.end())); 
+  vector_roll(y, -max_index+int(max_position_percentage*double(n)));
+
+  TGraph* g2 = new TGraph(n, &x[0], &y[0]);
+
+  return g2;
+}
+
+void g_normalize(TGraph* g, double norm=1.){
+  double max = TMath::MaxElement(g->GetN(), g->GetY());
+  for (int i=0; i<g->GetN(); i++) g->SetPointY(i, g->GetPointY(i)*norm/max);
+}
